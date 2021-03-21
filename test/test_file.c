fffcbd54f0cbb4c180e0e444cfea538cd32c57e6
erikgustavalm <erikgustavalm@Gmail.com>
2021-03-21 21:29:20 +0100

    You can try parsing now with =make tests= (#3)
    * You can try parsing now with =make tests=
    
    Reports some errors, especially with pop from stack.
    
    * Small fixes, still doesn't parse correctly.
    
    * Added easy test "x x"
    
    It clear that

--- test/test_file.c
+++ test/test_file.c
@@ -1,6 +1,14 @@
 #include <CUnit/Basic.h>
 
 #include "../src/heap.h"
+#include "../src/name.h"
+#include "../src/input.h"
+#include "../src/parse.h"
+#include "../src/types.h"
+
+#include <string.h>
+
+extern char * strdup(const char *);
 
 int init_suite(void)
 {
@@ -12,12 +20,48 @@ int clean_suite(void)
     return 0;
 }
 
-const char *test1_desc = "Test 1: check if starting and destroying the heap works\n";
+const char *test1_desc = "1: check if starting and destroying the heap works\n";
 void test1(void)
 {
     CU_ASSERT_PTR_NULL(heap);
     heap_setup();
     CU_ASSERT_PTR_NOT_NULL(heap);
+    memory_free();
+}
+
+const char *test2_desc = "2: parse easy input \"x x\"";
+void test2(void)
+{
+    heap_setup();
+    setup_names();
+
+    struct string_handle *sh = new_string_handle(strdup("x x"));
+    CU_ASSERT_PTR_NOT_NULL(sh);
+    struct input_handle *ih = input_from_string(sh);
+    CU_ASSERT_PTR_NOT_NULL(ih);
+    struct node result = parse_node(ih);
+    /* Don't know what it should be, leaf, branch or what */
+
+    free_string_handle(sh);
+    free_names();
+    memory_free();
+}
+
+const char *test3_desc = "3: parse easy input \"\\f.x\"";
+void test3(void)
+{
+    heap_setup();
+    setup_names();
+
+    struct string_handle *sh = new_string_handle(strdup("\\f.x"));
+    CU_ASSERT_PTR_NOT_NULL(sh);
+    struct input_handle *ih = input_from_string(sh);
+    CU_ASSERT_PTR_NOT_NULL(ih);
+    struct node result = parse_node(ih);
+    /* Don't know what it should be, leaf, branch or what */
+
+    free_string_handle(sh);
+    free_names();
     memory_free();
 }
 
@@ -35,7 +79,10 @@ int main(void)
 	    CU_cleanup_registry();
 	    return CU_get_error();
     }
-    if (!CU_add_test(test_suite, test1_desc, test1)) {
+    if (!CU_add_test(test_suite, test1_desc, test1)
+	|| !CU_add_test(test_suite, test2_desc, test2)
+	|| !CU_add_test(test_suite, test3_desc, test3)
+	) {
 	    CU_cleanup_registry();
 	    return CU_get_error();
     }
