49a7063abd9d825c5c8e673202d8c8db9cd5eb2c
August-Alm <augustalm@gmail.com>
2022-01-19 13:31:35 +0100

    stack overflow

--- src/memory.c
+++ src/memory.c
@@ -13,6 +13,7 @@
 void memory_setup()
 {
     heap_setup();
+    cleanup_stack_setup();
     free_stack_setup();
     names_setup();
     norm_stack_setup();
@@ -23,6 +24,7 @@ void memory_setup()
 void memory_free()
 {
     heap_free();
+    cleanup_stack_free();
     free_stack_free();
     names_free();
     norm_stack_free();
